2e2ddfa645517d5f2f61793081f342537b874634
Akansha1234pro <73756275+Akansha1234pro@users.noreply.github.com>
2021-10-01 21:51:57 +0530

    Update Min steps to gold.cpp

--- CPP/Min steps to gold.cpp
+++ CPP/Min steps to gold.cpp
@@ -1,6 +1,6 @@
 int Solution::coverPoints(vector<int> &A, vector<int> &B) {
-    int x=A[0], y=B[0], sum=0;
-    for(int i=1;i<A.size();i++)
+    long long x=A[0], y=B[0], sum=0;
+    for(long long i=1;i<A.size();i++)
     {
         if(abs(B[i]-y)>abs(A[i]-x))
         {
